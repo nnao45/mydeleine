437b4aed2f909a006b71d10aae7d9420810e0a19
nnao45 <n4sekai5y@gmail.com>
2019-02-11 23:38:01 +0900

    haribote

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use gtk::prelude::*;
 fn main() {
     gtk::init().expect("Failed to initialize GTK.");
 
-    let ui = include_str!("../ui/glade_window_1.ui");
+    let ui = include_str!("../ui/dialog_window_01.ui");
     let builder = gtk::Builder::new_from_string(ui);
     
-    let window1 : gtk::Window = builder.get_object("window_1").unwrap();
+    let window1 : gtk::Window = builder.get_object("dialog_window_01").unwrap();
     window1.connect_delete_event(move |_, _| {
         gtk::main_quit();
         Inhibit(false)
